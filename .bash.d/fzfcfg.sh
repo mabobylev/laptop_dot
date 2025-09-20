ae5bfd86b61ffe501ef5c79db184ce0806fd6de4
Mihail Bobylev <mabobylev@gmail.com>
2025-09-20 15:03:48 +0400

    изменено:      .bash.d/fzfcfg.sh 	изменено:      .config/sway/config

--- .bash.d/fzfcfg.sh
+++ .bash.d/fzfcfg.sh
@@ -1,16 +1,22 @@
 #######################################################
 # FZF settings
 #######################################################
-export FZF_DEFAULT_COMMAND='fd --type f'
-export FZF_DEFAULT_OPTS='
+export FZF_DEFAULT_COMMAND="fd --type f"
+export FZF_DEFAULT_OPTS="
   --layout=reverse
-  --info=inline
-  --border
-  --pointer 
   --color=fg:#ECEFF4,fg+:#EBCB8B,bg+:#3B4252
   --color=hl:#81A1C1,hl+:#5E81AC,info:#A3BE8C,marker:#3B4252
-  --color=pointer:#BF616A,spinner:#af5fff,prompt:#876253,header:#87afaf'
+  --color=pointer:#BF616A,spinner:#af5fff,prompt:#876253,header:#87afaf"
+# --info=inline
+# --border
+# --pointer 
 # --height 40%
+# export FZF_DEFAULT_OPTS=" \
+# --color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 \
+# --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+# --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+# --color=selected-bg:#45475a \
+# --color=border:#313244,label:#cdd6f4"
 # Preview file content using bat
 export FZF_CTRL_T_OPTS="
   --walker-skip .git,node_modules,target,PortProton
